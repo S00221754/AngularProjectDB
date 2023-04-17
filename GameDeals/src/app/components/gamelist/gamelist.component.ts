4c8cba0eabe248425d0265e3dfcf90e761875dae
tshilton583 <s00221754@mail.itsligo.ie>
2023-04-17 12:29:53 +0100

    added getdeals method

--- GameDeals/src/app/components/gamelist/gamelist.component.ts
+++ GameDeals/src/app/components/gamelist/gamelist.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
+import { GameDeal } from 'src/app/interface/game';
+import { GamedealApiService } from 'src/app/service/gamedeal-api.service';
 @Component({
   selector: 'app-gamelist',
   templateUrl: './gamelist.component.html',
   styleUrls: ['./gamelist.component.css']
 })
-export class GamelistComponent {
+export class GamelistComponent implements OnInit {
+  gamesData:GameDeal | any;
+  
 
+  constructor(private _gamedealApiService:GamedealApiService){}
+
+  ngOnInit(){
+    this.getDeals();
+  }
+
+  getDeals(){
+    this._gamedealApiService.getGameDeals().subscribe(gamesData =>
+      { this.gamesData = gamesData
+    });
+  }
 }
--- GameDeals/src/app/service/gamedeal-api.service.ts
+++ GameDeals/src/app/service/gamedeal-api.service.ts
@@ -1,9 +1,28 @@
 import { Injectable } from '@angular/core';
 
+import{HttpClient, HttpErrorResponse} from "@angular/common/http";
+import { Observable, throwError, catchError,tap } from 'rxjs';
+import { GameDeal } from '../interface/game';
 @Injectable({
   providedIn: 'root'
 })
 export class GamedealApiService {
 
-  constructor() { }
+  private _apiURL="https://www.cheapshark.com/api/1.0/";
+  private _deals="deals?"
+  constructor(private _http:HttpClient) { }
+
+  getGameDeals():Observable<any>{
+    return this._http.get<GameDeal>(this._apiURL + this._deals)
+    .pipe(
+      tap(data => console.log('game data/error') + JSON.stringify(data)
+      ),
+      catchError(this.handleError)
+    )
+  }
+  
+  private handleError (err:HttpErrorResponse) {
+    console.log('CarApiService: ' + err.message);
+    return err.message;
+  }
 }
