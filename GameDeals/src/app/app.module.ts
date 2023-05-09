6201d41f4b8d4aaa93edd87967d16a8e91b8aa2c
tshilton583 <s00221754@mail.itsligo.ie>
2023-05-09 20:43:37 +0100

    added httpclientmodule in appmodule

--- GameDeals/src/app/app.module.ts
+++ GameDeals/src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { GamelistComponent } from './components/gamelist/gamelist.component';
@@ -18,7 +18,8 @@ import { AboutComponent } from './components/about/about.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
