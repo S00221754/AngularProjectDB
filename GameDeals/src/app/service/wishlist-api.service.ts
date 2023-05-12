d47f9b25ef202b7d14cb7a5640ef6e017e5ebc70
tshilton583 <s00221754@mail.itsligo.ie>
2023-05-12 05:38:11 +0100

    added methods related to mongodb

--- GameDeals/src/app/service/wishlist-api.service.ts
+++ GameDeals/src/app/service/wishlist-api.service.ts
@@ -7,6 +7,38 @@ import { GameDeal } from '../interface/game';
 })
 export class WishlistApiService {
 
-  constructor() { }
+  private _siteURL="http://localhost:5050/games/"
 
+  constructor(private _http:HttpClient) { }
+
+  getWishListItem():Observable<any>{
+    return this._http.get<GameDeal>(this._siteURL)
+    .pipe(
+      tap(listData => console.log('list data/error' + JSON.stringify(listData))
+      ),
+      catchError(this.handleError)
+    );
+  }
+
+  addGame(game:GameDeal):Observable<any>{
+    return this._http.post<GameDeal>(this._siteURL, game)
+    .pipe(
+      tap(listData => console.log('added to wishlist/error' + JSON.stringify(listData))
+      ),
+      catchError(this.handleError)
+    )
+  }
+  deleteGame(gameID:GameDeal):Observable<any>{
+    let deleteURL=this._siteURL+":"+gameID //might need to get rid of colon for this to work
+    return this._http.delete(deleteURL)
+    .pipe(
+      tap(listData => console.log('deleted from wishlist/error' + JSON.stringify(listData))
+      ),
+      catchError(this.handleError)
+    )
+  }
+  private handleError (err:HttpErrorResponse) {
+    console.log('CarApiService: ' + err.message);
+    return err.message;
+  }
 }
